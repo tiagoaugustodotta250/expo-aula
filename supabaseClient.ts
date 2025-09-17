d4aef5e70a918c4981230aea0cb906b70ee5b4ef
tiago-dotta <tiagoaugustodotta250@gmail.com>
2025-09-16 20:55:39 -0400

    Add files via upload

---
+++ supabaseClient.ts
@@ -0,0 +1,7 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL!;
+const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY!;
+
+//Esse export será usado para que formulário seja utilizada a variável supabase
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
