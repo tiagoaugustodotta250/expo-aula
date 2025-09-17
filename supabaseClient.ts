46b280e85a62724e415eb7bb93aa9074950cc7f0
tiago-dotta <tiagoaugustodotta250@gmail.com>
2025-09-16 20:57:07 -0400

    Update supabaseClient.ts

--- supabaseClient.ts
+++ supabaseClient.ts
@@ -1,7 +1,9 @@
+//npm install @supabase/supabase-js
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL!;
 const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY!;
 
 //Esse export será usado para que formulário seja utilizada a variável supabase
+
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
